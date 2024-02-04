3481bd4474517f87edf3be145da53302754682fc
aritra <aritra@kali.kali>
2024-02-04 18:40:55 +0530

    changed logic for finding the qa.txt from user logged in first to whether the qa.txt file exists or not

--- mfa.sh
+++ mfa.sh
@@ -5,38 +5,9 @@ trap "" INT
 hello=`whoami`
 echo hello "$hello"
 tries=0
-remaining=3
-find qa.txt &>error.txt
-error=`cat error.txt`
-if [ "$loginattempts" -eq 1 ]
-then       	
-echo Since you are logging for the first time/there is no MFA file, you need to provide a question as well as answer for MFA
-echo Please enter your MFA question
-read question
-echo Please enter your MFA answer
-read answer
-echo "Your question and answer will be stored in a file called qa.txt in your home directory, please don't delete or modify it"
-touch "qa.txt"
-echo "$question" >> qa.txt
-echo "$answer" >> qa.txt
-echo "Your question and answer are successfully saved, from next time you need to provide MFA question and answer"
-stty start undef
-trap - INT
-elif [ "$error" == "find: ‘qa.txt’: No such file or directory" ]
-then
-  echo Since you are logging for the first time/there is no MFA file, you need to provide a question as well as answer for MFA
-  echo Please enter your MFA question
-  read question
-  echo Please enter your MFA answer
-  read answer
-  echo "Your question and answer will be stored in a file called qa.txt in your home directory, please don't delete or modify it"
-  touch "qa.txt"
-  echo "$question" >> qa.txt
-  echo "$answer" >> qa.txt
-  echo "Your question and answer are successfully saved, from next time you need to provide MFA question and answer"
-  stty start undef
-  trap - INT
-else
+remaining=3	
+if [ -f qa.txt ]
+then	
 while [ "$tries" -ne 3 ]
  do
    q=`cat qa.txt | head -n 1`
@@ -44,12 +15,12 @@ while [ "$tries" -ne 3 ]
    echo please enter your answer
    read a
    ans=`cat qa.txt | tail -n 1`
-   if [ "$a" == "$ans" ]
-   then
-      echo "answer is correct, you may proceed"
-      stty start undef
-      trap - INT
-      break
+   if [ "$a" = "$ans" ]
+   then	   
+   echo "answer is correct, you may proceed"
+   stty start undef
+   trap - INT
+   break
    else
       tries=$((tries +1))
       remaining=$((remaining -1))
@@ -61,6 +32,19 @@ while [ "$tries" -ne 3 ]
       fi
    fi
   done
+else
+echo Since you are logging for the first time/there is no MFA file, you need to provide a question as well as answer for MFA
+echo Please enter your MFA question
+read question
+echo Please enter your MFA answer
+read answer
+echo "Your question and answer will be stored in a file called qa.txt in your home directory, please don't delete or modify it"
+touch "qa.txt"
+echo "$question" >> qa.txt
+echo "$answer" >> qa.txt
+echo "Your question and answer are successfully saved, from next time you need to provide MFA question and answer"
+stty start undef
+trap - INT     	
 fi
 if [ "$tries" -eq 3 ]
 then
